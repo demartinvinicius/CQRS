371b897df66b4ec2334fb48c80278cc8f69e2ac7
Vinicius <demartinvinicius@gmail.com>
2022-03-09 09:52:23 -0300

    Primeiro Commit

---
+++ CrudCQRS/CQRS/Commands/DeleteProductByIdCommand.cs
@@ -0,0 +1,30 @@
+﻿using CrudCQRS.Models;
+using MediatR;
+
+
+namespace CrudCQRS.CQRS.Commands
+{
+    public class DeleteProductByIdCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, int>
+        {
+            private ProductContext context;
+            public DeleteProductByIdCommandHandler(ProductContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<int> Handle(DeleteProductByIdCommand command,CancellationToken cancellationToken)
+            {
+                var product = context.Product.Where(a => a.Id == command.Id).FirstOrDefault();
+                if (product == null)
+                    return default;
+
+                context.Product.Remove(product);
+                await context.SaveChangesAsync();
+                return product.Id;
+            }
+        }
+    }
+}
