83a554e7ca46e0022ca715f6d2a2253d8bee2020
Vinicius <demartinvinicius@gmail.com>
2022-03-10 15:50:00 -0300

    Criado Validador para bogus

---
+++ CrudCQRS/Features/Product/Bogus/CreateBogusValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CrudCQRS.Features.Product.Bogus;
+
+public class CreateBogusValidator : AbstractValidator<CreateBogusRequest>
+{
+    public CreateBogusValidator()
+    {
+        RuleFor(d => d.NumberItensToGenerate).GreaterThanOrEqualTo(0);
+        RuleFor(d => d.NumberItensToGenerate).LessThanOrEqualTo(1000);
+
+    }
+}
